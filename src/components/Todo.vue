87c50bcd091335350257e372f2f423d4f63c52f2
Arie <91861324+Arie75@users.noreply.github.com>
2022-02-26 22:03:52 +0700

    Update Todo.vue

--- src/components/Todo.vue
+++ src/components/Todo.vue
@@ -1,5 +1,5 @@
 <template>
-        <input type="text" v-model="title" @keyup.enter="addtodo" placeholder="todo here" class="border bg-[#F5F5F5] font-pupylinux w-[280px] py-2 px-3 ml-[30px] mt-9 rounded-md shadow mb-10">
+        <input type="text" v-model="title" @keyup.enter="addtodo" required placeholder="todo here" class="border bg-[#F5F5F5] font-pupylinux w-[280px] py-2 px-3 ml-[30px] mt-9 rounded-md shadow mb-10">
 
         <div v-for="todo in todos" :key="todo.id" class="border border-black w-[90%] mx-auto mb-4 py-3 pl-3 rounded-lg shadow transition-all dark:bg-slate-200">
             <p class="font-pupylinux pr-3">{{ todo.title }}</p>
